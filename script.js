50f82061a6786ea1ffb4c9c0ec59d9f91962381e
007 <yamatsuba008@gmail.com>
2020-05-27 11:49:54 +0300

    create random generator background and update code

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-var csscode = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradientbg");
-
-console.log(csscode);
-console.log(color1);
-console.log(color2);
+const csscode = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradientbg");
 
 function setGragient(){
     body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")"; 
@@ -13,5 +9,25 @@ function setGragient(){
 }
 
 color1.addEventListener("input",setGragient);
-
 color2.addEventListener("input",setGragient);
+
+const setBg = () => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.background = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+    return randomColor;
+}
+
+genNew.addEventListener("click", setBg);
+
+const createRandomGradient= () => {
+    const randomColor1 = setBg();
+    const randomColor2= setBg(); 
+    body.style.background = "linear-gradient(to right," + "#" + randomColor1 + ", " + "#" + randomColor2 + ")"; 
+    csscode.textContent = body.style.background + ";";
+}
+
+genNew.addEventListener("click", createRandomGradient);
+createRandomGradient();
+
+
